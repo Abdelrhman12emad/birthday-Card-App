1dd9cecf18d515caf66274b060528a579ce0267e
Abdelrhman12emad <167641233+Abdelrhman12emad@users.noreply.github.com>
2024-08-06 16:46:06 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0XFFD2BCD5),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/birthday_card.jpg'),
+        ),
+      ),
+    ));
+  }
+}
